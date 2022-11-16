06ad59a9545f30e61ef22297becc5961d1e4804f
Tristan F <LeoDog896@hotmail.com>
2022-11-16 18:40:06 -0500

    Standardize code format

--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,88 @@
 enum Axis {
-  X = 0,
-  Y = 1,
-  Z = 2
+	X = 0,
+	Y = 1,
+	Z = 2,
 }
 
 type Sides = ["F", "B", "U", "D", "L", "R"];
 type Side = Sides[number];
 
 interface Move {
-  side: Side,
-  axis: Axis,
-  count: number,
+	side: Side;
+	axis: Axis;
+	count: number;
 }
 
 function moveToString(move: Move): string {
-  let res = move.side;
-  switch (move.count) {
-    case 2:
-      res += '2';
-      break;
-    case 3:
-      res += '\'';
-      break;
-  }
-  return res;
+	let res = move.side;
+	switch (move.count) {
+		case 2:
+			res += "2";
+			break;
+		case 3:
+			res += "'";
+			break;
+	}
+	return res;
 }
 
 function sideAxis(side: Side): Axis {
-  switch (side) {
-    case 'L':
-    case 'R':
-      return Axis.X;
-    case 'U':
-    case 'D':
-      return Axis.Y;
-    case 'F':
-    case 'B':
-      return Axis.Z;
-    default:
-      const _exhaust: never = side
-      return _exhaust
-  }
+	switch (side) {
+		case "L":
+		case "R":
+			return Axis.X;
+		case "U":
+		case "D":
+			return Axis.Y;
+		case "F":
+		case "B":
+			return Axis.Z;
+		default:
+			const _exhaust: never = side;
+			return _exhaust;
+	}
 }
 
 function* generateMove(): Generator<Move, never, never> {
-  let lastMove: Move = {
-    side: "F",
-    axis: sideAxis("F"),
-    count: 1
-  }
+	let lastMove: Move = {
+		side: "F",
+		axis: sideAxis("F"),
+		count: 1,
+	};
 
-  const sides: Sides = ["F", "B", "U", "D", "L", "R"]
+	const sides: Sides = ["F", "B", "U", "D", "L", "R"];
 
-  while (true) {
-    let availableSides = sides.filter(side => {
-      console.log(side, sideAxis(side));
-      return sideAxis(side) != sideAxis(lastMove.side)
-    })
-    const randomSide = availableSides[Math.floor(Math.random() * availableSides.length)]
-    let move: Move = {
-      side: randomSide,
-      axis: sideAxis(randomSide),
-      count: Math.floor(Math.random() * 3) + 1
-    }
-    lastMove = move
-    yield move
-  }
+	while (true) {
+		let availableSides = sides.filter((side) => {
+			console.log(side, sideAxis(side));
+			return sideAxis(side) != sideAxis(lastMove.side);
+		});
+		const randomSide =
+			availableSides[Math.floor(Math.random() * availableSides.length)];
+		let move: Move = {
+			side: randomSide,
+			axis: sideAxis(randomSide),
+			count: Math.floor(Math.random() * 3) + 1,
+		};
+		lastMove = move;
+		yield move;
+	}
 }
 
 function generateScramble(count: number): string {
-  const generator = generateMove();
-  return Array.from(Array(count), () => generator.next(), generator) // generator.next() is called `count` times
-    .map(o => moveToString(o.value)) // convert values to move string
-    .join(" ")
+	const generator = generateMove();
+	return Array.from(Array(count), () => generator.next(), generator) // generator.next() is called `count` times
+		.map((o) => moveToString(o.value)) // convert values to move string
+		.join(" ");
 }
 
-const scramble = document.querySelector<HTMLParagraphElement>('#scramble')!
-const button = document.querySelector<HTMLButtonElement>('#button')!
+const scramble = document.querySelector<HTMLParagraphElement>("#scramble")!;
+const button = document.querySelector<HTMLButtonElement>("#button")!;
 
 function reset() {
-  scramble.textContent = generateScramble(20);
+	scramble.textContent = generateScramble(20);
 }
 
 button.onclick = reset;
 
-reset()
+reset();
